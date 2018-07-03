9df2892b52d744632dab78923d9e70489f94684d
bwegrzanowski <bartosz.wegrzanowski@gmail.com>
2018-07-03 14:25:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring exercises
+
+Exercises to get to know spring according to  [Spring Framework 5 - Beginner to Guru](https://courses.springframework.guru/p/spring-framework-5-begginer-to-guru) online course
+
---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,89 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String latName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String firstName, String latName) {
+        this.firstName = firstName;
+        this.latName = latName;
+    }
+
+    public Author(String firstName, String latName, Set<Book> books) {
+        this.firstName = firstName;
+        this.latName = latName;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLatName() {
+        return latName;
+    }
+
+    public void setLatName(String latName) {
+        this.latName = latName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", latName='" + latName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
